a3238905f0572f468d2960338354e4749ad0a9fa
BT_4N2_19 <egrkjbr@jdfgh.com>
2024-03-06 14:03:53 -0500

    final2

--- src/Class.java
+++ src/Class.java
@@ -1,25 +1,18 @@
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Scanner;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
+public class Class {
 
-        String[][] maze = getMaze("src/maze");
-        ArrayList<String> coordinates = new ArrayList<String>();
+    private ArrayList<String> coordinates;
+    int r = 0;
+    int c = 0;
+    private String[][] m;
+    public Class(String[][] m){
+        this.m = m;
+        this.r = r;
+        this.c = c;
+    }
+    public void runMaze(){
         boolean end = false;
-        System.out.println();
-        for (int r = 0; r < maze.length; r++) {
-            for (int c = 0; c < maze[0].length; c++) {
-                System.out.print(maze[r][c] + " ");
-            }
-            System.out.println();
-        }
-        int r = 0;
-        int c = 0;
         while(end==false){
             System.out.println();
             if(c==maze[0].length-1 && r==maze.length-1){
@@ -56,6 +49,7 @@ public class Main {
         }
         System.out.println(coordinates);
     }
+
     public static boolean checkRight(int r, int c, String[][] maze){
         if(c+1<maze[0].length && maze[r][c+1].equals(".")){
             return true;
@@ -95,36 +89,5 @@ public class Main {
             }
         }
         return maze;
-    }
-
-
-    public static String[][] getMaze(String fileName) {
-        File f = new File(fileName);
-        Scanner s = null;
-        try {
-            s = new Scanner(f);
-        }
-        catch (FileNotFoundException e) {
-            System.out.println("File not found.");
-            System.exit(1);
-        }
-
-        ArrayList<String> fileData = new ArrayList<String>();
-        while (s.hasNextLine())
-            fileData.add(s.nextLine());
-
-        int rows = fileData.size();
-        int cols = fileData.get(0).length();
-
-        String[][] maze = new String[rows][cols];
-
-        for (int i = 0; i < fileData.size(); i++) {
-            String d = fileData.get(i);
-            for (int j = 0; j < d.length(); j++) {
-                maze[i][j] = d.charAt(j) + "";
-            }
-        }
-        return maze;
-
     }
 }
